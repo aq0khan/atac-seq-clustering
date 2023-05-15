1328a65ac50771586db1eeea4db67b00c3e9f4a8
aq0khan <118698973+aq0khan@users.noreply.github.com>
2023-05-15 16:53:42 +0200

    Update script.py

--- agglomerative_clustering/script.py
+++ agglomerative_clustering/script.py
@@ -7,10 +7,10 @@ from sklearn.metrics import *
 from sklearn.metrics import normalized_mutual_info_score as NMI
 import time
 import tracemalloc
-file_names = ["adrenal","cerebellum","cerebrum","eye","heart","intestine", "kidney", "liver","lung", "muscle","pancreas","placenta", "spleen", "stomach", "thymus"]
+file_names = [""]
 
-LSI_PATH = "/work/lect0077/ATAC/h5ad/LSI/"
-PCA_PATH = "/hpcwork/lect0077/ATAC/h5ad/PCA/"
+LSI_PATH = ""
+PCA_PATH = ""
 
 def agglomerative_cluster(name, path):
     adata = ad.read(path + name + ".h5ad")
